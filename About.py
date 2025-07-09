33d27676de11e60fe061cea89de47a523894d8fa
ganesh.kumar@kofax.com <ganesh.kumar@kofax.com>
2025-07-09 14:34:13 +0530

    added the about page

---
+++ About.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("About This App")
+
+st.write("""
+This is a simple Todo app built with Streamlit.
+- You can add new todos
+- Mark todos as complete
+- Remove todos from the list
+""")
+st.write("This app uses a custom functions module to handle todo operations.")
--- web.py
+++ web.py
@@ -6,17 +6,25 @@ st.title("Hello, Streamlit!")
 
 todos = Functions.read_todos()
 
+st.set_page_config(page_title="Todo App",
+    page_icon=":clipboard:",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
 if not todos:
     st.write("No todos found. Please add some.")
 
 def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
+    todo = st.session_state["new_todo"]
     # pdb.set_trace()  # Uncomment this line to set a breakpoint for debugging
     if not todo.strip():  # todo empty check
         st.warning("Please enter a todo.")
         return
     todos.append(todo)
     Functions.write_todos(todos)
+
+new_todo = st.text_input(label="", placeholder="Add a new todo..", key="new_todo",on_change=add_todo)
 
 
 st.write("## Your Todos")
@@ -29,4 +37,4 @@ for index, todo in enumerate(todos):
         st.experimental_rerun()  # Refresh the page to update the todo list
 
 
-new_todo = st.text_input(label="", placeholder="Add a new todo..", key="new_todo",on_change=add_todo)
+
